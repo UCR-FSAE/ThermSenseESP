86da35fa848dd5930f9a6bd1a3a96ca9dfec8fc8
neeleshroy06 <neelesh.roy06@gmail.com>
2026-05-05 22:06:14 -0700

    z

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,7 @@ void setup() {
   pinMode(FAULT_PIN, OUTPUT_OPEN_DRAIN);
   digitalWrite(FAULT_PIN, HIGH);  // set to high to disable the fault pin, it is active low
   digitalWrite(MUX_ENBLE, LOW);  // Enables the multiplexer
-  // digitalWrite(MUX_S3, HIGH);
-  // digitalWrite(MUX_S2, LOW);
-  // digitalWrite(MUX_S1, HIGH);
-  // digitalWrite(MUX_S0, HIGH);
+
   Serial.begin(115200);
   delay(500);
   analogReadResolution(12);
@@ -67,14 +64,14 @@ void setup() {
     while (1);
   }
 
-  switch (subpackID) {
-    case 1: CAN_outMsg1.id = 0x11; CAN_outMsg2.id = 0x12; break;
-    case 2: CAN_outMsg1.id = 0x21; CAN_outMsg2.id = 0x22; break;
-    case 3: CAN_outMsg1.id = 0x31; CAN_outMsg2.id = 0x32; break;
-    case 4: CAN_outMsg1.id = 0x41; CAN_outMsg2.id = 0x42; break;
-    case 5: CAN_outMsg1.id = 0x51; CAN_outMsg2.id = 0x52; break;
-    case 6: CAN_outMsg1.id = 0x61; CAN_outMsg2.id = 0x62; break;
-  }
+  // switch (subpackID) {
+  //   case 1: CAN_outMsg1.id = 0x11; CAN_outMsg2.id = 0x12; break;
+  //   case 2: CAN_outMsg1.id = 0x21; CAN_outMsg2.id = 0x22; break;
+  //   case 3: CAN_outMsg1.id = 0x31; CAN_outMsg2.id = 0x32; break;
+  //   case 4: CAN_outMsg1.id = 0x41; CAN_outMsg2.id = 0x42; break;
+  //   case 5: CAN_outMsg1.id = 0x51; CAN_outMsg2.id = 0x52; break;
+  //   case 6: CAN_outMsg1.id = 0x61; CAN_outMsg2.id = 0x62; break;
+  // }
 
   CAN_outMsg1.len = CAN_outMsg2.len = 6;
 
@@ -85,70 +82,57 @@ void loop() {
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
   uint8_t temp[12] = {0};
 
-  for (int i=0; i<12;i++) {
-    int channel = (i+1)%12;  // T12 has binary b0000, others are fine
-    // selecting the mux channel
-    digitalWrite(MUX_S0, channel%2);
-    digitalWrite(MUX_S1, (channel/2)%2);
-    digitalWrite(MUX_S2, (channel/4)%2);
-    digitalWrite(MUX_S3, (channel/8)%2);
+  // for (int i=0; i<12;i++) {
+  //   int channel = (i+1)%12;  // T12 has binary b0000, others are fine
+  //   // selecting the mux channel
+  //   digitalWrite(MUX_S0, channel%2);
+  //   digitalWrite(MUX_S1, (channel/2)%2);
+  //   digitalWrite(MUX_S2, (channel/4)%2);
+  //   digitalWrite(MUX_S3, (channel/8)%2);
 
-    // this delay is needed for the mux to settle after switching channels, otherwise the ADC reading will be unstable and inaccurate
-    // there might be channel bleeding
-    // without this, the channel numbers were mismatching
-    delayMicroseconds(20);
+  //   // this delay is needed for the mux to settle after switching channels, otherwise the ADC reading will be unstable and inaccurate
+  //   // there might be channel bleeding
+  //   // without this, the channel numbers were mismatching
+  //   delayMicroseconds(20);
     
-    // // discard the first reading after switching channels, it is usually inaccurate due to the residual charge in the ADC sample and hold capacitor from the previous channel
-    analogRead(ADC_PIN);
-    delayMicroseconds(20);
-    // reduce noise by taking average
-    int32_t raw =0;
-    for (int j=0;j<16;j++) {
-      raw += analogRead(ADC_PIN);
-    }
-    adcValue = raw/16.0f;
+  //   // // discard the first reading after switching channels, it is usually inaccurate due to the residual charge in the ADC sample and hold capacitor from the previous channel
+  //   analogRead(ADC_PIN);
+  //   delayMicroseconds(20);
+  //   // reduce noise by taking average
+  //   int32_t raw =0;
+  //   for (int j=0;j<16;j++) {
+  //     raw += analogRead(ADC_PIN);
+  //   }
+  //   adcValue = raw/16.0f;
 
-    if (adcValue<=0 || adcValue>=ADC_MAX) {
-      Serial.println("Error: thermistor open or short circuit!");
-      delay(SEND_DELAY);
-      temp[i] = 0;
-      continue;
-    }
+  //   if (adcValue<=0 || adcValue>=ADC_MAX) {
+  //     Serial.println("Error: thermistor open or short circuit!");
+  //     delay(SEND_DELAY);
+  //     temp[i] = 0;
+  //     continue;
+  //   }
 
-    float R = R_FIXED *adcValue/(ADC_MAX - adcValue);
-    float lnR = logf(R);
-    float inv_T = SH_A + SH_B*lnR + SH_C*lnR*lnR*lnR;
-    temperature = 1.0f/inv_T - 273.15f;
+  //   float R = R_FIXED *adcValue/(ADC_MAX - adcValue);
+  //   float lnR = logf(R);
+  //   float inv_T = SH_A + SH_B*lnR + SH_C*lnR*lnR*lnR;
+  //   temperature = 1.0f/inv_T - 273.15f;
 
-    if(temperature > fault_temp){
-      Serial.println("Over temperature detected on channel ");
-      Serial.println(i+1);
-      digitalWrite(FAULT_PIN, LOW);  // set fault pin low to indicate over-temperature, it is active low
-      Serial.println("Fault pin set LOW");
-    }
-    temp[i] = (uint8_t)(temperature);
+  //   if(temperature > fault_temp){
+  //     Serial.println("Over temperature detected on channel ");
+  //     Serial.println(i+1);
+  //     digitalWrite(FAULT_PIN, LOW);  // set fault pin low to indicate over-temperature, it is active low
+  //     Serial.println("Fault pin set LOW");
+  //   }
+  //   temp[i] = (uint8_t)(temperature);
 
-    Serial.print("Channel: ");
-    Serial.print(i+1);
-    Serial.print(" | Temp: ");
-    Serial.print(temperature, 2);
-    Serial.println(" C");
-    delay (100);
-  }
-
-  //   CAN_outMsg1.buf[i] = (uint8_t)temperature;
+  //   Serial.print("Channel: ");
+  //   Serial.print(i+1);
+  //   Serial.print(" | Temp: ");
+  //   Serial.print(temperature, 2);
+  //   Serial.println(" C");
+  //   delay (100);
   // }
-  // CAN_outMsg1.buf[0] = (uint8_t)temperature; 
 
-  // Serial.print("ADC Raw: ");
-  // Serial.print(adcValue, 0);
-  // Serial.print("  |  R: ");
-  // Serial.print(R, 1);
-  // Serial.print(" ohm  |  Temp: ");
-  // Serial.print(temperature, 2);
-  // Serial.println(" C");
-
-  // Can.write(CAN_outMsg1);
 
   for (int i = 0; i < 6; i++) {
       CAN_outMsg1.buf[i] = temp[i];

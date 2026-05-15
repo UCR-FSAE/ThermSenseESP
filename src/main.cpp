94e73fbf83d297ece2e3e3ec7ed242d607eb5925
neeleshroy06 <neelesh.roy06@gmail.com>
2026-05-14 17:50:29 -0700

    fault works

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <math.h>
 #include "driver/twai.h"
 
-static const gpio_num_t CAN_TX = GPIO_NUM_43;  // D6
-static const gpio_num_t CAN_RX = GPIO_NUM_44;  // D7
+static const gpio_num_t CAN_TX = GPIO_NUM_4;  // D3
+static const gpio_num_t CAN_RX = GPIO_NUM_5;  // D4
 
 static constexpr uint8_t ADC_PIN = GPIO_NUM_8;  // D9 or A9
 
@@ -14,9 +14,9 @@ static constexpr uint8_t MUX_S1 = GPIO_NUM_2;  // D1
 static constexpr uint8_t MUX_S0 = GPIO_NUM_38;  // D11
 
 static constexpr uint8_t FAULT_PIN = GPIO_NUM_6;  // D5
-static constexpr float FAULT_TEMP = 60.0f;
+static constexpr float FAULT_TEMP = 20.0f;
 
-static constexpr uint32_t SEND_DELAY_MS = 1000;
+static constexpr uint32_t SEND_DELAY_MS = 5;
 
 static constexpr float SH_A = 1.1395e-3f;
 static constexpr float SH_B = 2.3230e-4f;
@@ -31,6 +31,8 @@ static twai_message_t CAN_outMsg2;
 static float adcValue = 0;
 static float temperature = 0;
 
+static bool twai_initialized = false;
+
 static bool init_can() {
   twai_general_config_t g = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX, CAN_RX, TWAI_MODE_NO_ACK);
   g.tx_queue_len = 25;
@@ -39,6 +41,8 @@ static bool init_can() {
   twai_timing_config_t t = TWAI_TIMING_CONFIG_500KBITS();
   twai_filter_config_t f = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
+  twai_driver_uninstall();
+
   esp_err_t err = twai_driver_install(&g, &t, &f);
   Serial.printf("twai_driver_install: %s\n", esp_err_to_name(err));
   if (err != ESP_OK) return false;
@@ -46,6 +50,8 @@ static bool init_can() {
   err = twai_start();
   Serial.printf("twai_start: %s\n", esp_err_to_name(err));
   if (err != ESP_OK) return false;
+
+  twai_initialized = true;
 
   return true;
 }
@@ -74,7 +80,7 @@ void setup() {
   pinMode(MUX_S1, OUTPUT);
   pinMode(MUX_S0, OUTPUT);
   pinMode(FAULT_PIN, OUTPUT_OPEN_DRAIN);
-  digitalWrite(FAULT_PIN, HIGH);
+  digitalWrite(FAULT_PIN, LOW);
   digitalWrite(MUX_ENBLE, LOW);
 
   Serial.begin(115200);
@@ -87,6 +93,9 @@ void setup() {
     while (1) {}
   }
 
+  memset(&CAN_outMsg1, 0, sizeof(CAN_outMsg1));
+  memset(&CAN_outMsg2, 0, sizeof(CAN_outMsg2));
+
   switch (subpackID) {
     case 1: CAN_outMsg1.identifier = 0x11; CAN_outMsg2.identifier = 0x12; break;
     case 2: CAN_outMsg1.identifier = 0x21; CAN_outMsg2.identifier = 0x22; break;
@@ -95,7 +104,8 @@ void setup() {
     case 5: CAN_outMsg1.identifier = 0x51; CAN_outMsg2.identifier = 0x52; break;
     case 6: CAN_outMsg1.identifier = 0x61; CAN_outMsg2.identifier = 0x62; break;
   }
-  CAN_outMsg1.data_length_code = CAN_outMsg2.data_length_code = 6;
+  CAN_outMsg1.data_length_code = 6;
+  CAN_outMsg2.data_length_code = 6;
 }
 
 void loop() {
@@ -122,7 +132,7 @@ void loop() {
     delayMicroseconds(20);
 
     temperature = read_temperature();
-    if (temperature > FAULT_TEMP) digitalWrite(FAULT_PIN, LOW);
+    if (temperature > FAULT_TEMP) digitalWrite(FAULT_PIN, HIGH);
     temp[i] = (uint8_t)temperature;
   }
 
@@ -130,12 +140,28 @@ void loop() {
     CAN_outMsg1.data[i] = temp[i];
     CAN_outMsg2.data[i] = temp[i + 6];
   }
+  
+  if(twai_initialized) {
+    esp_err_t err1 = twai_transmit(&CAN_outMsg1, pdMS_TO_TICKS(10));
+    esp_err_t err2 = twai_transmit(&CAN_outMsg2, pdMS_TO_TICKS(10));
+    // twai_status_info_t st;
+    // if (twai_get_status_info(&st) == ESP_OK && st.state == TWAI_STATE_BUS_OFF) {
+    if(err1 == ESP_ERR_INVALID_STATE || err2 == ESP_ERR_INVALID_STATE) {
+      Serial.println("Bus off detected before transmission, resetting TWAI...");
+      twai_initialized = false;
+      twai_stop();
+      twai_driver_uninstall();
+      delay(10);
+      init_can();
+    } else {
+      Serial.println(err1 == ESP_OK ? "MSG1 sent" : "MSG1 FAILED");
+      Serial.println(err2 == ESP_OK ? "MSG2 sent" : "MSG2 FAILED");
+    }
+  }
+  // esp_err_t err1 = twai_transmit((twai_message_t*)&CAN_outMsg1, pdMS_TO_TICKS(10));
+  // esp_err_t err2 = twai_transmit((twai_message_t*)&CAN_outMsg2, pdMS_TO_TICKS(10));
 
-  esp_err_t err1 = twai_transmit((twai_message_t*)&CAN_outMsg1, pdMS_TO_TICKS(10));
-  esp_err_t err2 = twai_transmit((twai_message_t*)&CAN_outMsg2, pdMS_TO_TICKS(10));
-
-  Serial.println(err1 == ESP_OK ? "MSG1 sent" : "MSG1 FAILED");
-  Serial.println(err2 == ESP_OK ? "MSG2 sent" : "MSG2 FAILED");
+  
 
   Serial.print("MSG1 (0x");
   Serial.print(CAN_outMsg1.identifier, HEX);
